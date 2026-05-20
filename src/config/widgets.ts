2d83e32682c7edfdb7db3fa6c381c339f597ea46
Nick <a1-6@Nick-MacBook-Air-4.local>
2026-05-21 00:48:03 +0800

    feat: remove social links from profile widget

--- src/config/widgets.ts
+++ src/config/widgets.ts
@@ -29,33 +29,7 @@ export const profileConfig: ProfileConfig = {
 		enable: true,
 		speed: 80,
 	},
-	links: [
-		{
-			name: "Bilibili",
-			icon: "fa7-brands:bilibili",
-			url: "https://space.bilibili.com/701864046",
-		},
-		{
-			name: "Gitee",
-			icon: "mdi:git",
-			url: "https://gitee.com/matsuzakayuki",
-		},
-		{
-			name: "GitHub",
-			icon: "fa7-brands:github",
-			url: "https://github.com/matsuzaka-yuki",
-		},
-		{
-			name: "Codeberg",
-			icon: "simple-icons:codeberg",
-			url: "https://codeberg.org",
-		},
-		{
-			name: "Discord",
-			icon: "fa7-brands:discord",
-			url: "https://discord.gg/MqW6TcQtVM",
-		},
-	],
+	links: [],
 };
 
 /**
